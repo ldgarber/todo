89db396e60af412a3183106f0cb60598c3beabe9
Leah D. Garber <leah.garber88@gmail.com>
2021-03-25 18:01:30 -0400

    react app with amplify backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
+Auth.configure(awsconfig);
+API.configure(awsconfig);
+
+function updateTodo(todo, newDesc) {
+  todo['description'] = newDesc;
+  API.graphql(graphqlOperation(mutations.updateTodo, {input: todo}));
+}
+
+function deleteTodo(todo) {
+  API.graphql(graphqlOperation(mutations.deleteTodo, {input: {'id': todo['id']}}));
+}
+
+function todoByName(name) {
+  return API.graphql(graphqlOperation(queries.listTodos, {filter: {'name':{'eq':name}}}));
+}
 
 function App() {
+
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos));
+  console.log(todoByName("leah.garber88@gmail.com"));
+
+  /*
+  Auth.currentAuthenticatedUser({
+    bypassCache: false
+  }).then(function(user) {
+    console.log("User: " + JSON.stringify(user));
+    const todo = { name: user['username'], description: "new todo" };
+    const newTodo = API.graphql(graphqlOperation(mutations.createTodo, {input: todo}));
+  }).catch(err => console.log(err));
+  */
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +57,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
